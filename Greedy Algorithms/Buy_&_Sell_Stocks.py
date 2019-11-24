65ca1e97255e71fd1015f3640203cb8107041809
Matthew Guest <matthewguest.dev@gmail.com>
2019-11-23 22:56:07 -0800

    Updated buy and sell stocks.

--- Greedy Algorithms/Buy_&_Sell_Stocks.py
+++ Greedy Algorithms/Buy_&_Sell_Stocks.py
@@ -1,25 +1,23 @@
 # Brute Force
-def maxProfit(self, prices):
-    max_profit = 0
+def maxProfit(prices):
+  max_profit = 0
 
-    for i in range(0, len(prices):
-        for j in range(0, len(prices):
-            profit = prices[j] - prices[i]
-            if profit > max_profit:
-                max_profit = profit
-    return max_profit
+  for i in range(0, len(prices)):
+      for j in range(0, len(prices)):
+          profit = prices[j] - prices[i]
+          if profit > max_profit:
+              max_profit = profit
+  return max_profit
 
 # Final Solution
-def maxProfit(self, prices):
-    if len(prices)==0: return 0
-    if len(prices)==1: return 0
-        curMax=prices[len(prices)-1]
-        maxP=0
-        for i in range(len(prices)):
-            temp=prices[len(prices)-i-1]
-            if temp>curMax:
-                curMax=temp
-            curProfit=curMax-temp
-            if curProfit>maxP:
-                maxP=curProfit
-        return maxP
+def maxProfit(prices):
+  if len(prices) <= 1:
+      return 0
+  profit = 0
+  low = prices[0]
+  for i in range(1,len(prices)):
+      if prices[i] < low:
+          low = prices[i]
+      else:
+          profit = max(prices[i]-low, profit)
+  return profit
