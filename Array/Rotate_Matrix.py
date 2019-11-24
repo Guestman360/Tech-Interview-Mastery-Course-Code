8aa97e8c94d0c17a2039d38a45fdb94df02094ed
Matthew Guest <matthewguest.dev@gmail.com>
2019-11-23 21:52:27 -0800

    Fixed formatting for one question.

--- Array/Rotate_Matrix.py
+++ Array/Rotate_Matrix.py
@@ -1,16 +1,14 @@
 def rotateMatrix(matrix, N):
 
-	for i in range(0, N // 2):
+  for i in range(0, N // 2):
 
-		j = i
+    j = i
+    for j in range(j, N - i - 1):
+      temp = matrix[i][j]
 
-    	for j in range(j, N - i - 1):
-    
-            temp = matrix[i][j]
+      matrix[i][j] = matrix[N - 1 - j][i]
+      matrix[N - 1 - j][i] = matrix[N - 1 - i][N - 1 - j]
+      matrix[N - 1 - i][N - 1 - j] = matrix[j][N - 1 - i]
+      matrix[j][N - 1 - i] = temp
 
-            matrix[i][j] = matrix[N - 1 - j][i]
-            matrix[N - 1 - j][i] = matrix[N - 1 - i][N - 1 - j] 
-            matrix[N - 1 - i][N - 1 - j] = matrix[j][N - 1 - i]
-            matrix[j][N - 1 - i] = temp
-
-    return matrix
+  return matrix
